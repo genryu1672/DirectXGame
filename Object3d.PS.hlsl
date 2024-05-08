65171a395fe469151357c4213bcc4b08924b669b
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2024-05-08 16:55:35 +0900

    02_00    P20まで

---
+++ Object3d.PS.hlsl
@@ -0,0 +1,15 @@
+struct Pixelshaderoutput {
+float32_t4color:SV_TARGET0;
+};
+
+Pixelshaderoutput main() {
+	Pixelshaderoutput output;
+	output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
+	return output;
+}
+
+
+
+
+
+
