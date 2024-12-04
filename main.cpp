b67598d7483e22d4936ed3c655e85b80e29cc7b0
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2024-12-04 10:41:16 +0900

    CG3_01_00までやった。

--- main.cpp
+++ main.cpp
@@ -537,7 +537,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {//main関数
 	//ウィンドウの生成
 	HWND hwnd = CreateWindow(
 		wc.lpszClassName,
-		L"CG2",
+		L"LE2D_10_シマノ_ユウト",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
