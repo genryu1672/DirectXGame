d0aba17fdfad062cbda7f41565725e312f49838b
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-01-23 12:32:14 +0900

    CG2_05_03P18までやった。

--- Resources/Shaders/Object3d.VS.hlsl
+++ Resources/Shaders/Object3d.VS.hlsl
@@ -19,6 +19,7 @@ VertexShaderOutput main(VertexShaderInput input) {
 	VertexShaderOutput output;
 	output.position =mul(input.position, gTransformationMatrix.WVP) ;//output.position = input.position
 	output.texcoord = input.texcoord;
+	output.normal = normalize(mul(input.normal, (float32_t3x3)gTransformationMatrix.World));
 	return output;
 }
 
